14d9651c33c14d77b51d84a9ceb19dc4567910e6
asddssaa28 <zxccxxzz28@gmail.com>
2025-05-21 17:28:26 +0800

    Delete pfa-interview-site-final directory

--- pfa-interview-site-final/components/ui/card.tsx
+++
@@ -1,7 +0,0 @@
-
-export function Card({ children, className }: any) {
-  return <div className={`rounded-2xl shadow-md ${className}`}>{children}</div>;
-}
-export function CardContent({ children, className }: any) {
-  return <div className={className}>{children}</div>;
-}
